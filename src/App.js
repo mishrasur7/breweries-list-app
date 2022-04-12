bd1e4d3f5acbaf00073919d1eb85cdaa85b7a80f
Suraj Mishra <mishrasur7@gmail.com>
2022-04-12 15:01:14 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Full Stack pre-assignment</h1>
+    </div>
+  );
+}
+
+export default App;
